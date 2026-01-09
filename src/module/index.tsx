5648003f593e1c9c0e27c822b35f56b1318e3ac2
beardkoda <akinsuyi.joshua84@gmail.com>
2026-01-09 12:20:20 +0200

    feat: added version to the sdk example

--- src/module/index.tsx
+++ src/module/index.tsx
@@ -10,27 +10,35 @@ import { ConfigProvider } from "./_context";
 import TenderWidget from "./screens";
 import { TenderAgentProps } from "./types";
 
-const TenderAgentSdk = ({ referenceId, amount, accessId, accessSecret, env, fiatCurrency, paymentExpirySeconds, onEventResponse }:TenderAgentProps) => {
-  return (
-    <div id="tender-cash-agent-sdk" className="tender-cash-agent-sdk">
-      {createPortal(
-      <ConfigProvider
-        config={{
-          referenceId,
-          accessId,
-          accessSecret,
-          amount,
-          fiatCurrency,
-          env,
-          paymentExpirySeconds,
-          onEventResponse
-        }}
-      >
-        <TenderWidget />
-      </ConfigProvider>
-      , document.body)}
-    </div>
-  )
-}
+const TenderAgentSdk = ({
+    referenceId,
+    amount,
+    accessId,
+    accessSecret,
+    env,
+    fiatCurrency,
+    paymentExpirySeconds,
+    onEventResponse,
+}: TenderAgentProps) => {
+    return createPortal(
+        <div id="tender-cash-agent-sdk" className="tender-cash-agent-sdk">
+            <ConfigProvider
+                config={{
+                    referenceId,
+                    accessId,
+                    accessSecret,
+                    amount,
+                    fiatCurrency,
+                    env,
+                    paymentExpirySeconds,
+                    onEventResponse,
+                }}
+            >
+                <TenderWidget />
+            </ConfigProvider>
+        </div>,
+        document.body
+    );
+};
 
 export default TenderAgentSdk;
